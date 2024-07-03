71955decdadd45be1411322ed29d7cdc9d482089
sistemas-snte56 <computo.seccional@outlook.com>
2024-07-03 14:03:06 -0600

    Antes de hacer migracion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,50 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            // #Campos originales
+            // $table->id();
+            // $table->string('name');
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->rememberToken();
+            // $table->foreignId('current_team_id')->nullable();
+            // $table->string('profile_photo_path', 2048)->nullable();
+            // $table->timestamps();
+
+
+            #Modificamos los campos que se requieren en Users
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('id_delegacion');
+            $table->unsignedBigInteger('id_secretaria');
+            $table->string('titulo' , 150);
+            $table->string('nombre' , 150);
+            $table->string('apaterno' , 150);
+            $table->string('amaterno' , 150);
+            $table->unsignedBigInteger('id_genero');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            
+            $table->string('telefono' , 20)->nullable()->default(null);
+            $table->string('direccion' ,250)->nullable()->default(null);
+            $table->string('cp' , 50)->nullable()->default(null);
+            $table->string('ciudad' , 50)->nullable()->default(null);
+            $table->string('estado' , 50)->nullable()->default(null);
+            
             $table->timestamps();
+
+
+
+
+
+
+
+
         });
     }
 
